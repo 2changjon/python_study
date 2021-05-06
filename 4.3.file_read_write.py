940956dc67a3efbd8ac8e95112bf31be72e2664f
2changjon <2changjon@naver.com>
2021-05-06 13:53:09 +0900

    시험 끝

--- 4.3.file_read_write.py
+++ 4.3.file_read_write.py
@@ -1,5 +1,12 @@
 # 파일 객체 = open(파일 이름, 파일 열기 모드) r,w,a(읽고 쓰고 추가)
 
+# 쓰기
+f = open("C:\python_study\새파일.txt", 'w')
+for i in range(1, 11):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
+f.close()
+
 # 읽기 전체1
 f = open("C:\python_study\새파일.txt", 'r')
 while True:
@@ -15,24 +22,17 @@ for line in lines:
     print("lines", line)
 f.close()
 
-# 읽기 전체3
-f = open("C:\python_study\새파일.txt", 'r')
-data = f.read()
-print("data", data)
-f.close()
-
-# 쓰기
-f = open("C:\python_study\새파일.txt", 'w')
-for i in range(1, 11):
-    data = "%d번째 줄입니다.\n" % i
-    f.write(data)
-f.close()
-
 # 추가
 f = open("C:\python_study\새파일.txt", 'a')
 for i in range(11, 20):
     data = "%d번째 줄입니다.\n" % i
     f.write(data)
+f.close()
+
+# 읽기 전체3
+f = open("C:\python_study\새파일.txt", 'r')
+data = f.read()
+print("data", data)
 f.close()
 
 # whth문 사용시 자동 close
@@ -43,4 +43,7 @@ with open("foo.txt", "r") as f:
     print(f.read())
 
 with open("foo.txt", "a") as f:
-    f.write("end")
+    f.write(" end")
+
+with open("foo.txt", "r") as f:
+    print(f.read())
